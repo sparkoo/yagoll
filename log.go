4eb03144579ebfe48b8a6ce28e83362c26a28393
Michal Vala <michal.vala@gmail.com>
2018-10-01 15:39:52 +0200

    log to stderr, include line prefix

--- log.go
+++ log.go
@@ -1,11 +1,31 @@
 package yagoll
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+var out = os.Stdout
+var err = os.Stderr
+
+func prefix() string {
+	_, file, line, ok := runtime.Caller(2)
+	if ok {
+		filePath := strings.Split(file, "/")
+		fileName := filePath[len(filePath)-1]
+		return fmt.Sprint("[ ", time.Now().Format(time.RFC3339), " ", fileName, ":", line, " ] ")
+	} else {
+		panic("failed")
+	}
+}
 
 func Println(msg ... interface{}) {
-	log.Println(msg)
+	err.Write([]byte(fmt.Sprint(prefix(), msg, "\n")))
 }
 
 func Printf(msg string, args ... interface{}) {
-	log.Printf(msg, args)
+	err.Write([]byte(fmt.Sprint(prefix(), fmt.Sprintf(msg, args))))
 }
